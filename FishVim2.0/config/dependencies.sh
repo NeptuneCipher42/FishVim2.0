bb73c9cbe09551d8beea51ae852f9c2b80a7a282
Nicholas Fisher <143345524+NeptuneCipher42@users.noreply.github.com>
2026-02-21 00:32:40 -0500

    Delete FishVim2.0 directory

--- FishVim2.0/config/dependencies.sh
+++
@@ -1,9 +0,0 @@
-sudo apt install ripgrep fd-find nodejs npm python3-pip xclip -y
-sudo apt install pipx -y
-pip install pynvim
-sudo apt install golang-go
-sudo npm install -g eslint_d
-LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
-curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-tar xf lazygit.tar.gz lazygit
-sudo install lazygit -D -t /usr/local/bin/
